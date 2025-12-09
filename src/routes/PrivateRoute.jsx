800d6498b46842faa6921222747fefb632feb403
istiaquerijve <istiaquerijve@gmail.com>
2025-12-09 19:23:54 +0600

    privateRoute

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import useAuth from '../hooks/useAuth';
+import { Navigate } from 'react-router';
+
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useAuth();
+
+    if(loading){
+        return <div className="">
+            <span className="loading loading-dots loading-xl"></span>
+        </div>
+    }
+
+    if(!user){
+        return <Navigate to="/register"></Navigate>
+    }
+
+    return children;
+};
+
+export default PrivateRoute;
